00d9c717a3f5c108a960fde642ec1f378624e751
Sami Elsaeyed <samiabdu.11377@gmail.com>
2021-01-08 08:22:38 -0500

    Hover

--- script.js
+++ script.js
@@ -1,2 +1,8 @@
 document.getElementsByTagName("button")[0].addEventListener("click",function(event){document.getElementById("one").innerHTML= "I am right";document.getElementById("two").innerHTML= ""});
 document.getElementsByTagName("button")[1].addEventListener("click",function(event){document.getElementById("two").innerHTML= "No I am right";document.getElementById("one").innerHTML= ""});
+document.getElementById("hover").addEventListener("mouseover",function func(event){alert("Hey! I told You no tto Hover Over Me!")});
+
+
+
+
+
