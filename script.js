4d7bae0585cdb482ec7b2532f2fc56c609bf0c26
Sami Elsaeyed <samiabdu.11377@gmail.com>
2021-01-08 09:45:33 -0500

    Prompt 5

--- script.js
+++ script.js
@@ -1,6 +1,10 @@
 document.getElementsByTagName("button")[0].addEventListener("click",function(event){document.getElementById("one").innerHTML= "I am right";document.getElementById("two").innerHTML= ""});
 document.getElementsByTagName("button")[1].addEventListener("click",function(event){document.getElementById("two").innerHTML= "No I am right";document.getElementById("one").innerHTML= ""});
 document.getElementById("hover").addEventListener("mouseover",function func(event){alert("Hey! I told You no tto Hover Over Me!")});
+if (document.getElementById("password").value===12345678) { 
+    document.getElementById("correct").innerHTML= "Dont worry its valid"; 
+    alert("Password not Valid");
+}
 
 
 
